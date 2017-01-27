4dbbe659866620903f7f67c1b1e2ac67a3880194
Ibad Qureshi <Ibad Qureshi>
2017-01-27 07:57:20 -0500

    adding authentication

--- app/src/main/java/com/example/dellpc/parkingmanagement/MainActivity.java
+++ app/src/main/java/com/example/dellpc/parkingmanagement/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.dellpc.parkingmanagement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button BtnLoginUser;
+    Button BtnLoginAdmin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BtnLoginUser = (Button) findViewById(R.id.BTN_UserLogin);
+        BtnLoginAdmin = (Button) findViewById(R.id.BTN_AdminLogin);
+
+        BtnLoginAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AdminActivity.class);
+                startActivity(intent);
+            }
+        });
+        BtnLoginUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UserActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
 
     }
 }
---
+++ app/src/main/java/com/example/dellpc/parkingmanagement/UserActivity.java
@@ -0,0 +1,111 @@
+package com.example.dellpc.parkingmanagement;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+
+public class UserActivity extends AppCompatActivity {
+    //Request code
+    //It is a flag which we'd placed uiAuth while we're logging in from diff UI.
+    public static final int RC_SIGN_IN = 1;
+    //entry point of a firebase
+    private FirebaseDatabase mFirebaseDatabase;
+
+    //referencing a specific database
+    private DatabaseReference mDatabaseReference;
+    //authenticating the state change
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseStorage mFirebaseStorage;
+    public static final String ANONYMOUS = "anonymous";
+    private String mUsername;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+        //main access point of our database
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseStorage = FirebaseStorage.getInstance();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser != null) {
+                    //user is signed in
+                    onSignedInInitialize(firebaseUser.getDisplayName());
+                    //Toast.makeText(MainActivity.this, "You're Signed in Welcome to the Friendly Chat App", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    //user is signed out
+
+                    onSignedOutCleanup();
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    //.setIsSmartLockEnabled(false)
+                                    .setProviders(
+                                            AuthUI.EMAIL_PROVIDER,
+                                            AuthUI.GOOGLE_PROVIDER,
+                                            AuthUI.FACEBOOK_PROVIDER
+                                    ).build(),
+                            RC_SIGN_IN);
+
+                }
+
+            }
+        };
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+
+                Log.d("Signin", "onActivityResult: " + " OK");
+                Toast.makeText(this, "Signed In", Toast.LENGTH_SHORT).show();
+            } else if (requestCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Sign in cancelled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mAuthStateListener != null) {
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+    private void onSignedInInitialize(String username) {
+        mUsername = username;
+    }
+    private void onSignedOutCleanup() {
+        mUsername = ANONYMOUS;
+    }
+
+
+}
