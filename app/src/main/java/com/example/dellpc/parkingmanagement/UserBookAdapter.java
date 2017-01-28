54b0f5a1467e66695614892c6a04cfa9229e836d
Ibad Qureshi <Ibad Qureshi>
2017-01-28 07:52:06 -0500

    setting the list views

--- app/src/main/java/com/example/dellpc/parkingmanagement/UserBookAdapter.java
+++ app/src/main/java/com/example/dellpc/parkingmanagement/UserBookAdapter.java
@@ -1,8 +1,41 @@
 package com.example.dellpc.parkingmanagement;
 
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
+
 /**
  * Created by dell pc on 28-Jan-17.
  */
 
-public class UserBookAdapter  {
+public class UserBookAdapter extends ArrayAdapter<UserBookClass> {
+
+    public UserBookAdapter(Context context, int resource, int textViewResourceId, UserBookClass[] objects) {
+        super(context, resource, textViewResourceId, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null){
+            convertView = ((Activity)getContext()).getLayoutInflater().inflate(R.layout.custom_user_info,parent,false);
+        }
+
+        TextView username = (TextView) convertView.findViewById(R.id.c_user_name);
+        TextView slotno = (TextView) convertView.findViewById(R.id.c_slot_no);
+        TextView time = (TextView) convertView.findViewById(R.id.c_tTime);
+
+        UserBookClass userBookClass = getItem(position);
+
+        username.setText(userBookClass.getUsername());
+        slotno.setText(userBookClass.getSlotno());
+       // time.setText(userBookClass.);
+
+        return convertView;
+    }
 }
--- app/src/main/java/com/example/dellpc/parkingmanagement/UserFBAdapter.java
+++ app/src/main/java/com/example/dellpc/parkingmanagement/UserFBAdapter.java
@@ -1,8 +1,33 @@
 package com.example.dellpc.parkingmanagement;
 
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
 /**
  * Created by dell pc on 28-Jan-17.
  */
 
-public class UserFBAdapter {
+public class UserFBAdapter extends ArrayAdapter<UerFBClass>{
+    public UserFBAdapter(Context context, int resource, int textViewResourceId, UerFBClass[] objects) {
+        super(context, resource, textViewResourceId, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null){
+            convertView = ((Activity)getContext()).getLayoutInflater().inflate(R.layout.custom_user_feedback,parent,false);
+        }
+        UerFBClass uerFBClass = getItem(position);
+
+        TextView userfeedbk = (TextView) convertView.findViewById(R.id.c_userfb);
+        userfeedbk.setText(uerFBClass.getUserFB());
+
+        return convertView;
+    }
 }
